b94a6d7382c836edf4e71e3e7e62a75e5dbd984b
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-13 08:32:29 +0800

    Commiting

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -46,6 +46,6 @@ def get_logger() -> logging.Logger:
         propagate=False,
     )
     formatter = RedactingFormatter(fields=PII_FIELDS)
-    logger.addHandler(formatter)
+    logger.addHandler(formatter.format)
     logger.addFilter(formatter.fields)
     return logger
