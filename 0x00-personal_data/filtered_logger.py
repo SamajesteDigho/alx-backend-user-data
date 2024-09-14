20e3dbf5800a46c38b917d5d7825472e7dcac946
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-15 03:58:22 +0800

    Commiting

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,7 +56,7 @@ def get_logger() -> logging.Logger:
 def get_db() -> connector.connection.MySQLConnection:
     """ Getting the database """
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
 
