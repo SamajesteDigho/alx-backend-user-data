35a8dbf6a55a231364a68ec390a9e720c5c005e8
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-18 16:24:19 +0800

    Commiting

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,14 @@ class BasicAuth(Auth):
     def __init__(self) -> None:
         """ Here the initialization """
         super().__init__()
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extraction base64 """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
