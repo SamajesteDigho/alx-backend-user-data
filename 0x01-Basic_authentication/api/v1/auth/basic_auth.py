9263757ef7941e80f07db47b38afa4b9a3a235a4
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-18 17:05:25 +0800

    Commiting

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,9 @@
 """ Basic Authentication module
 """
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
 from .auth import Auth
+from api.v1.views.users import User
 
 
 class BasicAuth(Auth):
@@ -50,3 +51,18 @@ class BasicAuth(Auth):
             return None, None
         data = decoded_base64_authorization_header.split(':')
         return data[0], data[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):  # type: ignore
+        """ Getting the user instance fom here """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search(attributes={'email': user_email})
+        if users is None or len(users) == 0:
+            return None
+        user = users[0]
+        if user.is_valid_password(user_pwd):
+            return None
+        return user
