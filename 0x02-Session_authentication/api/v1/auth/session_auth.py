b1c5b9a3256653e613b789f89eda114190659486
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-19 18:49:12 +0800

    Commiting

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,19 @@
 """ Basic Authentication module
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Session Auth class """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creating a session """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        key = str(uuid4())
+        self.user_id_by_session_id[key] = user_id
+        return key
